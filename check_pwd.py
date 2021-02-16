0c9abe18c96136f2ef1c4456b169db6180f65c6a
CDaudish <67439102+CDaudish@users.noreply.github.com>
2021-02-15 23:03:17 -0600

    modified function to accept ! as special char

--- check_pwd.py
+++ check_pwd.py
@@ -91,12 +91,17 @@ def check_pwd(password):
     if c == "@":
       atSign = True
 
+  exclaim = False
+  for c in password:
+    if c == "!":
+      exclaim = True
+
 
 
   if equalSign == False and minusSign == False and plusSign == False and underScore == False and  \
           rightBrac == False and leftBrac == False and star == False and ampersand == False and \
           upArrow == False and remainder == False and moneySign == False and hashtag == False and \
-          atSign == False:
+          atSign == False and exclaim == False:
     return False
 
 
