0735e651713f9aea7793b4630f3648508bfbe7b9
CDaudish <67439102+CDaudish@users.noreply.github.com>
2021-02-15 22:08:51 -0600

    Modified function so to return false if there are no uppercase characters in string

--- check_pwd.py
+++ check_pwd.py
@@ -1,12 +1,22 @@
 def check_pwd(password):
   if len(password) < 8:
     return False
+
   if len(password) > 20:
     return False
+
   isLowerCase = False
   for c in password:
     if c.islower() == True:
       isLowerCase = True
   if isLowerCase == False:
     return False
+
+  isUpperCase = False
+  for c in password:
+    if c.isupper() == True:
+      isUpperCase = True
+  if isUpperCase == False:
+    return False
+
   return True
